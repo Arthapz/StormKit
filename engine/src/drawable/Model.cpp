5effc09b192e0f4047cb3d0cd2716ad2fa4267a8
Arthur LAURENT <arthur.laurent4@gmail.com>
2020-05-22 15:03:40 +0000

    Using local tinygltf instead of installed version

--- engine/src/drawable/Model.cpp
+++ engine/src/drawable/Model.cpp
@@ -17,7 +17,7 @@
 #define TINYGLTF_USE_CPP14
 #define TINYGLTF_NOEXCEPTION
 #define JSON_NOEXCEPTION
-#include <tiny_gltf.h>
+#include "../tiny_gltf.h"
 
 using namespace storm;
 using namespace storm::engine;
