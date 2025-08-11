72cefb6b4a1ea6a79be7d06c078d30de7c7d6100
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-08-11 10:17:49 +0200

    (core) introduce windows.hpp

---
+++ include/stormkit/core/platform/windows.hpp
@@ -0,0 +1,12 @@
+#ifndef STORMKIT_ANTI_WINDOWS_HPP
+#define STORMKIT_ANTI_WINDOWS_HPP
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+#undef __nullnullterminated
+#undef DELETE
+
+#endif
