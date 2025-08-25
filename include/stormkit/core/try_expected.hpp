6312f80dffd65c39f04ee1f0f7f613e362b6ea8e
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-08-25 22:02:13 +0200

    feat(core) add try macro to improve expected usage and provide a coroutine fallback

---
+++ include/stormkit/core/try_expected.hpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2023 Arthur LAURENT <arthur.laurent4@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level of this distribution
+
+#ifndef STORMKIT_TRY_EXPECTED_HPP
+#define STORMKIT_TRY_EXPECTED_HPP
+
+#if (defined(__clang__) or defined(__GNUC__))
+    #define Try(m)                                      \
+        ({                                              \
+            auto res = (m);                             \
+            if (!res.has_value()) [[unlikely]]          \
+                return std::unexpected { res.error() }; \
+            std::move(res).value();                     \
+        })
+    #define Ret(x)    return x
+    #define RetErr(x) return std::unexpected { x };
+#else
+    #define Try(x)    co_await x
+    #define Ret(x)    co_return x
+    #define RetErr(x) co_return std::unexpected { x };
+#endif
+
+#endif
