629e751ed1a6d72485c176fd532b1ecfc56c6337
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-11-02 02:03:34 +0100

    nfc(engine) remove engine module (have it's own repo now)

--- xmake/options.lua
+++ xmake/options.lua
@@ -1,12 +1,4 @@
 ---------------------------- global options ----------------------------
-option("examples_engine", {
-    default = false,
-    category = "root menu/others",
-    deps = { "examples" },
-    after_check = function(option)
-        if option:dep("examples"):enabled() then option:enable(true) end
-    end,
-})
 option("examples_gpu", {
     default = false,
     category = "root menu/others",
@@ -59,25 +51,17 @@ option("image", { default = true, category = "root menu/modules" })
 option("main", { default = true, category = "root menu/modules" })
 option("wsi", { default = true, category = "root menu/modules" })
 option("gpu", { default = true, category = "root menu/modules", deps = { "log", "image", "wsi" } })
-option("engine", {
-    default = true,
-    category = "root menu/modules",
-    deps = { "log", "entities", "image", "wsi", "gpu" },
-})
 option("compile_commands", { default = false, category = "root menu/support" })
 option("vsxmake", { default = false, category = "root menu/support" })
 
 option("rad", { default = false, category = "root menu/others" })
 option("devmode", {
     category = "root menu/others",
-    deps = { "tests", "examples", "compile_commands", "mold", "sanitizers", "engine", "mode" },
+    deps = { "tests", "examples", "compile_commands", "mold", "sanitizers", "mode" },
     after_check = function(option)
         if option:enabled() then
             for _, name in ipairs({ "tests", "examples", "compile_commands", "mold", "sanitizers" }) do
                 option:dep(name):enable(true)
-            end
-            for _, name in ipairs({ "engine" }) do
-                option:dep(name):enable(false)
             end
         end
     end,
