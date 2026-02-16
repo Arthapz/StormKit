034cd70541f61440e16f2b3446cf966ddf341bf4
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-02-16 18:30:01 +0100

    (xmake) update rules

--- xmake/rules/stormkit_application.xmake.lua
+++ xmake/rules/stormkit_application.xmake.lua
@@ -1,11 +1,37 @@
 namespace("stormkit", function()
     rule("application", function()
-        add_deps("flags")
+        add_deps("@stormkit::flags")
 
         on_config(function(target)
+            import("core.base.hashset")
+
             local stormkit_components = target:values("stormkit.components") or {}
-            target:set("kind", "binary")
-            target:set("languages", "cxxlatest", "clatest")
+            local stormkit_components_set = hashset.from(stormkit_components)
+
+            -- core --
+            target:add("packages", "frozen", "unordered_dense", "tl_function_ref")
+
+            if stormkit_components_set:has("image") then target:add("packages", "libktx", "libpng", "libjpeg-turbo") end
+            if stormkit_components_set:has("wsi") then
+                if target:is_plat("linux") then
+                    target:add(
+                        "packages",
+                        "libxcb",
+                        "xcb-util-keysyms",
+                        "xcb-util",
+                        "xcb-util-image",
+                        "xcb-util-wm",
+                        "xcb-util-errors",
+                        "wayland",
+                        "wayland-protocols",
+                        "libxkbcommon"
+                    )
+                end
+            end
+            if stormkit_components_set:has("gpu") then
+                target:add("packages", "volk", "vulkan-headers", "vulkan-memory-allocator")
+            end
+
             target:add("packages", "stormkit", { components = table.join("core", "main", stormkit_components) })
         end)
     end)
--- xmake/rules/stormkit_library.xmake.lua
+++ xmake/rules/stormkit_library.xmake.lua
@@ -1,12 +1,38 @@
 namespace("stormkit", function()
     rule("library", function()
-        add_deps("flags")
+        add_deps("@stormkit::flags")
 
         on_config(function(target)
+            import("core.base.hashset")
+
             local stormkit_components = target:values("stormkit.components") or {}
-            if not target:kind() then target:set("kind", "$(kind)") end
-            target:set("languages", "cxxlatest", "clatest")
-            target:add("packages", "stormkit", { components = table.join("core", "main", stormkit_components) })
+            local stormkit_components_set = hashset.from(stormkit_components)
+
+            -- core --
+            target:add("packages", "frozen", "unordered_dense", "tl_function_ref")
+
+            if stormkit_components_set:has("image") then target:add("packages", "libktx", "libpng", "libjpeg-turbo") end
+            if stormkit_components_set:has("wsi") then
+                if target:is_plat("linux") then
+                    target:add(
+                        "packages",
+                        "libxcb",
+                        "xcb-util-keysyms",
+                        "xcb-util",
+                        "xcb-util-image",
+                        "xcb-util-wm",
+                        "xcb-util-errors",
+                        "wayland",
+                        "wayland-protocols",
+                        "libxkbcommon"
+                    )
+                end
+            end
+            if stormkit_components_set:has("gpu") then
+                target:add("packages", "volk", "vulkan-headers", "vulkan-memory-allocator")
+            end
+
+            target:add("packages", "stormkit", { components = table.join("core", stormkit_components) })
         end)
     end)
 end)
