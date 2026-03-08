7c3d1aad9d1d2ef99510699dc521e19c04d9decb
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-03-08 21:00:51 +0100

    (log) add macro to define log functions with a custom module

--- include/stormkit/log/log_macro.hpp
+++ include/stormkit/log/log_macro.hpp
@@ -13,8 +13,7 @@
         constexpr auto NAME = stormkit::log::Module { module_chars }; \
     }
 
-#define LOGGER(module)                                                        \
-    NAMED_LOGGER(LOG_MODULE, module)                                          \
+#define LOGGER_FUNC(LOG_MODULE)                                               \
     template<class... Args>                                                   \
     STORMKIT_FORCE_INLINE inline auto dlog(Args&&... args) noexcept -> void { \
         LOG_MODULE.dlog(std::forward<Args>(args)...);                         \
@@ -35,32 +34,17 @@
     STORMKIT_FORCE_INLINE inline auto flog(Args&&... args) noexcept -> void { \
         LOG_MODULE.flog(std::forward<Args>(args)...);                         \
     }
+
+#define LOGGER(module)               \
+    NAMED_LOGGER(LOG_MODULE, module) \
+    LOGGER_FUNC(LOG_MODULE)
 
 #define IN_MODULE_NAMED_LOGGER(NAME, module_chars) \
     [[maybe_unused]]                               \
     inline constexpr auto NAME = stormkit::log::Module { module_chars };
 
-#define IN_MODULE_LOGGER(module)                                              \
-    IN_MODULE_NAMED_LOGGER(LOG_MODULE, module)                                \
-    template<class... Args>                                                   \
-    STORMKIT_FORCE_INLINE inline auto dlog(Args&&... args) noexcept -> void { \
-        LOG_MODULE.dlog(std::forward<Args>(args)...);                         \
-    }                                                                         \
-    template<class... Args>                                                   \
-    STORMKIT_FORCE_INLINE inline auto ilog(Args&&... args) noexcept -> void { \
-        LOG_MODULE.ilog(std::forward<Args>(args)...);                         \
-    }                                                                         \
-    template<class... Args>                                                   \
-    STORMKIT_FORCE_INLINE inline auto wlog(Args&&... args) noexcept -> void { \
-        LOG_MODULE.wlog(std::forward<Args>(args)...);                         \
-    }                                                                         \
-    template<class... Args>                                                   \
-    STORMKIT_FORCE_INLINE inline auto elog(Args&&... args) noexcept -> void { \
-        LOG_MODULE.elog(std::forward<Args>(args)...);                         \
-    }                                                                         \
-    template<class... Args>                                                   \
-    STORMKIT_FORCE_INLINE inline auto flog(Args&&... args) noexcept -> void { \
-        LOG_MODULE.flog(std::forward<Args>(args)...);                         \
-    }
+#define IN_MODULE_LOGGER(module)               \
+    IN_MODULE_NAMED_LOGGER(LOG_MODULE, module) \
+    LOGGER_FUNC(LOG_MODULE)
 
 #endif
